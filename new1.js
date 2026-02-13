f360354b5d69732962266a4c119ee6093cf0bb69
vickysuresh70-ux <vickysuresh70@gmail.com>
2026-02-13 21:19:51 +0530

    Add files via upload

---
+++ new1.js
@@ -0,0 +1,18 @@
+const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".yes-btn");
+const music = document.getElementById("bg-music");
+
+// No button run away
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * window.innerWidth;
+  const y = Math.random() * window.innerHeight;
+
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+// Yes button click
+yesBtn.addEventListener("click", () => {
+  alert("Yayyy 😍 I Love You Too pujipa  💖");
+  music.play();
+});
